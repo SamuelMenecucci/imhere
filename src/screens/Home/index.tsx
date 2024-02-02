965a822ec9f1f1cfd15f9ef51d128bcdea522b40
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-02-02 00:02:30 -0300

    feat: created home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+
+export function Home() {
+  const handleParticipantAdd = () => {};
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+          style={styles.input}
+          keyboardType="default" //https://reactnative.dev/docs/textinput#keyboardtype
+        />
+
+        {/* https://reactnative.dev/docs/touchableopacity */}
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#fcfcfe",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16,
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: "#1f1e25",
+    borderRadius: 5,
+    color: "#fdfcfe",
+    padding: 16,
+    fontSize: 16,
+  },
+  buttonText: {
+    color: "#fcfcfe",
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31cf67",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  form: {
+    flexDirection: "row",
+    width: "100%",
+    gap: 12,
+    marginTop: 36,
+    marginBottom: 42,
+  },
+});
